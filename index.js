8a596f0276fba4af4555242b90dc1b070a4d37af
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-08-23 21:37:24 +0000

    index: debug every commands

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const prefix = require("./config.json").prefix;
 const whatsapp = require("@open-wa/wa-automate");
 const fs = require("fs");
+const moment = require("moment-timezone");
+moment.tz.setDefault("Asia/Jakarta").locale("id");
 
 const availableCommands = new Set();
 
@@ -29,9 +31,10 @@ let args;
 let command;
 
 function start(bot) {
+    console.log("[BOT] dvstBot is now running!");
     bot.onStateChanged(async state => {
         if (state === "CONFLICT" || state === "UNLAUNCHED") bot.forceRefocus();
-        console.log("[Client State]", state);
+        console.log("[CLIENT]", state);
     });
 
     bot.onMessage(async message => {
@@ -52,7 +55,15 @@ function start(bot) {
         // eslint-disable-next-line no-empty
         } catch {}
         if (availableCommands.has(command)) {
+            console.log(`[EXEC] ${message.chat.name || message.chat.formattedTitle}: ${sender}: ${command} [${args.length}] ${moment(message.t * 1000).format("DD/MM/YY HH:mm:ss")}`);
             require(`./commands/${command}`).run(bot, message, args);
         }
+    });
+
+    bot.onIncomingCall(async (callData) => {
+        await bot.sendText(callData.peerJid, "*Beep boop!*\nCan't receive call for now.\n\n```- dvstBot```")
+            .then(async () => {
+                await bot.contactBlock(callData.peerJid);
+            });
     });
 }
