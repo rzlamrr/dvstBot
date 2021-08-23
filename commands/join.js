2e108c2501459da9ff68b71dc8aa3a2ede1dfab3
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-08-23 05:34:32 +0000

    commands: new join, all cmd

---
+++ commands/join.js
@@ -0,0 +1,16 @@
+const { prefix } = require("../config.json");
+
+exports.run = async (bot, message) => {
+    const link = message.body.slice(6);
+    await bot.joinGroupViaLink(link)
+        .then(async () => {
+            await bot.reply(message.from, "Joined via invite link!", message.id);
+        });
+};
+
+exports.help = {
+    name: "Join",
+    description: "Join group via invite link.",
+    usage: `${prefix}join`,
+    cooldown: 25
+};
